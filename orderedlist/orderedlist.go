194d507df8407ec4c4d16e04b9479dd8d9897a6b
Preetam Jinka <pj@isomero.us>
2013-07-31 01:00:36 -0400

    Added documentation.

--- orderedlist/orderedlist.go
+++ orderedlist/orderedlist.go
@@ -1,3 +1,4 @@
+// Package orderedlist is a basic wrapper around container/list.
 package orderedlist
 
 import (
@@ -5,16 +6,19 @@ import (
 	"fmt"
 )
 
+// OrderedList is an ordered linked list.
 type OrderedList struct {
 	linkedlist *list.List
 }
 
+// New returns an initialized OrderedList.
 func New() *OrderedList {
 	return &OrderedList{
 		linkedlist: list.New(),
 	}
 }
 
+// Insert inserts a key string into the ordered list.
 func (l *OrderedList) Insert(key string) {
 	// Empty list or greatest key
 	if l.linkedlist.Len() == 0 || l.linkedlist.Back().Value.(string) < key {
@@ -31,6 +35,7 @@ func (l *OrderedList) Insert(key string) {
 	}
 }
 
+// Remove removes a key from the ordered list.
 func (l *OrderedList) Remove(key string) {
 	for e := l.linkedlist.Front(); e != nil; e = e.Next() {
 		if e.Value.(string) == key {
@@ -40,6 +45,8 @@ func (l *OrderedList) Remove(key string) {
 	}
 }
 
+// firstGreaterThanOrEqual returns the first Element
+// greater-than or equal-to the given key.
 func (l *OrderedList) firstGreaterThanOrEqual(key string) *list.Element {
 	elem := l.linkedlist.Front()
 	for e := elem; e != nil; e = e.Next() {
@@ -51,6 +58,7 @@ func (l *OrderedList) firstGreaterThanOrEqual(key string) *list.Element {
 	return elem
 }
 
+// GetRange returns a slice of strings from [start, end).
 func (l *OrderedList) GetRange(start string, end string) (keys []string) {
 	keys = make([]string, 0)
 	startElem := l.firstGreaterThanOrEqual(start)
@@ -62,6 +70,7 @@ func (l *OrderedList) GetRange(start string, end string) (keys []string) {
 	return
 }
 
+// Print prints the values stored in the list.
 func (l *OrderedList) Print() {
 	for e := l.linkedlist.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value)
