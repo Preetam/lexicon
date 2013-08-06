afc06865dae7dbff8e251b990c10db1c8554ffe6
Preetam Jinka <pj@isomero.us>
2013-08-06 19:42:17 -0400

    Added persistence.

--- lexicon_test.go
+++ lexicon_test.go
@@ -29,6 +29,31 @@ func TestGetRange(t *testing.T) {
 	}
 }
 
+func TestWriteRead(t *testing.T) {
+	lex := New()
+	lex.Set("foo", "bar")
+	lex.Set("foobar", "baz")
+	lex.Set("bar", "foo")
+	lex.Set("a", "1")
+
+	if err := lex.WriteToFile("/tmp/test.lex"); err != nil {
+		t.Error(err)
+	}
+
+	lex, err := ReadFromFile("/tmp/test.lex")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	kv := lex.GetRange("", "\xff")
+	if res := fmt.Sprint(kv); res != "[{a 1} {bar foo} {foo bar} {foobar baz}]" {
+		t.Errorf("Expected kv to be %v, got %v",
+			"[{a 1} {bar foo} {foo bar} {foobar baz}]",
+			res)
+	}
+}
+
 func BenchmarkBasicSetRemove(b *testing.B) {
 	lex := New()
 
